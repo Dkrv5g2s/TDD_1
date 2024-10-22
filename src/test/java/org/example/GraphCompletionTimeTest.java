1a7284892eb4c4aa2c9fdd12f42e7b89d75c635c
陳泰吉 <113598010@cc.ntut.edu.tw>
2024-10-22 13:47:17 +0800

    write testCyclicGraph

--- src/test/java/org/example/GraphCompletionTimeTest.java
+++ src/test/java/org/example/GraphCompletionTimeTest.java
@@ -21,4 +21,17 @@ class GraphCompletionTimeTest {
         int result = graph.calculateMaxCompletionTime(3);
         assertEquals(6, result); // 2 + 3 + 1 = 6
     }
+
+    @Test
+    public void testCyclicGraph() {
+        GraphCompletionTime graph = new GraphCompletionTime(2);
+        graph.setTimeRequired(1, 2);
+        graph.setTimeRequired(2, 3);
+        graph.addEdge(1, 2);
+        graph.addEdge(2, 1);
+
+        int result = graph.calculateMaxCompletionTime(2);
+        assertEquals(-1, result); // 應返回一個指示循環的值
+    }
 }
+
